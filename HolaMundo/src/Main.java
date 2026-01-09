29fb4f417a554ea45e803942a8e11aab8a6145eb
AlejandroSalgadoMX <20253ds149@utez.edu.mx>
2026-01-08 21:08:23 -0600

    Proyecto 01: hola mundo

---
+++ HolaMundo/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+            /*
+        Salgado Uresti Rene Alejandro
+        2E
+        08/01/2026
+         */
+        System.out.println("Hola mundo");
+
+    }
+}
