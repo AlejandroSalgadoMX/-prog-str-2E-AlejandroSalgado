13fc0b93d0c49b34372e0e68470eba5562592c18
Alejandro Salgado <20253ds149@utez.edu.mx>
2026-03-10 20:28:51 -0600

    Javadocs a la clase Main

--- demoListView/src/main/java/com/example/demolistview/Main.java
+++ demoListView/src/main/java/com/example/demolistview/Main.java
@@ -6,8 +6,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+/**
+ * Clase principal de la aplicación JavaFX.
+ * Extiende {@link Application} y define el metodo {@code start} que
+ * carga la interfaz desde un archivo FXML y muestra la ventana principal.
+ */
 
 public class Main extends Application {
+
+    /**
+     * Inicializa y muestra la ventana principal de la aplicación.
+     * @param stage Ventana principal de JavaFX
+     * @throws IOException Si ocurre un error al cargar el archivo FXML
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/example/demolistview/views/app-view.fxml"));
